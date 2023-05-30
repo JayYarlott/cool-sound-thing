2052b4d01863f3568288b170352fde912f3b5e0f
JayYarlott <jyarlo5728@inst.hcpss.org>
2023-05-30 14:10:31 -0400

    Rendering done :)

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -12,6 +13,21 @@ public class Main {
         thing.render(im);
     }
 
-
+    private void render(ArrayList<Particle> list, Graphics2D g){
+        //we assume list is sorted
+        for(int i = 0; i<list.size(); i++){
+            list.get(i).update();
+        }
+        int x1 = (int) list.get(0).x;
+        int y1 = (int) list.get(0).y;
+        for(int i = 1; i<list.size(); i++){
+            int x2 = (int) list.get(i).x;
+            int y2 = (int) list.get(i).y;
+            g.drawLine(x1, y1, x2, y2);
+            x1 = x2;
+            y1 = y2;
+        }
+        g.drawLine(x1, y1, (int) list.get(0).x, (int) list.get(0).y);
+    }
 
 }
